de7bb38158d4ec444a2a97538e6bc0c8e3660633
ozunaj <ozunaj@uthscsa.edu>
2017-10-05 12:40:33 -0500

    Fixed positioning issues & syntax
    I fixed the issues with the CSS. I commented out the parts causing the issues so you can review. I also added in a bleed and roomy class. We use these alot in our themes. Lastly, I updated the syntax in your html to my preference but I am not saying it was wrong before. Let me know if you have any questions

--- page-templates/custom-theme.php
+++ page-templates/custom-theme.php
@@ -3,29 +3,15 @@
 Template Name: Custom Theme
 */
 get_header(); ?>
-
-	<?php do_action( 'foundationpress_before_content' ); ?>
-
-
-	<div class="grid-x">
+<section class="white bleed">
+	<div class="grid-x roomy">
 		<div class="cell">
-			<section class="wrapping" style="background-color:powderblue;">
-				<?php get_template_part( 'template-parts/featured-image' ); ?>
-				<div class="text">
-					<h1 class="entry-title custom-font">
-						John Ryan
-					</h1>
-					<h6 class="sans-font">
-						This is my first website!
-					</h6>
-					<a class="hollow button sans-font" href="#">
-			Button
-		</a>
-				</div>
-			</section>
+			<div class="text">
+				<h1 class="entry-title custom-font">John Ryan</h1>
+				<h6 class="sans-font">This is my first website!</h6>
+				<a class="hollow button sans-font" href="#">Button</a>
+			</div>
 		</div>
 	</div>
-
-	<?php do_action( 'foundationpress_after_content' ); ?>
-
-	<?php get_footer();
+</section>
+<?php get_footer();
